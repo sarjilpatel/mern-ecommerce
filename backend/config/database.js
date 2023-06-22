0e713a2dd2734422bc800fb938d9a24ba2cb9954
sarjilpatel <sarjilpatel2903@gmail.com>
2023-06-22 21:22:54 +0530

    added await in database connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
-const connectDatabase = () => {
-  mongoose
+const connectDatabase = async () => {
+  await mongoose
     .connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
