61528970f2385590c1ed8338f377e015ce793da7
sarjilpatel <sarjilpatel2903@gmail.com>
2023-06-22 20:47:11 +0530

    added catch in database connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,6 +9,9 @@ const connectDatabase = () => {
     })
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
+    })
+    .catch((err) => {
+      console.log(err);
     });
 };
 
